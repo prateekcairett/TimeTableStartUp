aea622f4be7c226421368cdd4e2b9d14aa2eda3e
Prateek Caire <timetablestartup@gmail.com>
2015-08-31 23:11:57 -0700

    Latest Code

--- src/Permute/SpacedOut.java
+++ src/Permute/SpacedOut.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -68,7 +69,7 @@ public class SpacedOut {
 		Arrays.sort(chars);
 		String sub_node_structure_sorted = new String(chars);
 		CombinatorialScore combinatorial_score = m_best_combinatorial_scores.get(sub_node_structure_sorted);
-		System.out.println("Sorted from BestCombinatoriaScore : " + sub_node_structure_sorted);
+//		System.out.println("Sorted from BestCombinatoriaScore : " + sub_node_structure_sorted);
 		return combinatorial_score;
 	}
 	
@@ -90,16 +91,16 @@ public class SpacedOut {
 		SpacedOut spaced_out = new SpacedOut();
 		//spaced_out.CheckPassbyValue(lecture_list);
 		HashMap<String,Integer> subject_count = new HashMap<String, Integer>();
-		subject_count.put("Maths", 3);
-		subject_count.put("English", 2);
-		subject_count.put("SST", 2);
-		subject_count.put("Computer Science", 1);
-		subject_count.put("Physics", 1);
-		subject_count.put("Chemistry", 1);
-		subject_count.put("Sub1", 1);
-		subject_count.put("Sub2", 1);
-		subject_count.put("Sub3", 1);
-		subject_count.put("Sub4", 1);
+		subject_count.put("Maths", 8);
+		subject_count.put("English", 7);
+		subject_count.put("SST", 6);
+		subject_count.put("ComputerScience", 5);
+		subject_count.put("Physics", 5);
+		subject_count.put("Chemistry", 4);
+//		subject_count.put("Sub1", 1);
+//		subject_count.put("Sub2", 1);
+//		subject_count.put("Sub3", 1);
+//		subject_count.put("Sub4", 1);
 		spaced_out.SingleClassTimeTables(subject_count);
 		System.out.println(spaced_out.m_permuation_count);
 	}
@@ -112,11 +113,106 @@ public class SpacedOut {
 	}*/
 
 	private void SingleClassTimeTables(HashMap<String, Integer> subject_count_map) {
-		ArrayList<Integer> lecture_list = CreateLectureList(subject_count_map);
+		ArrayList<Integer> lecture_list = CreateLectureList2(subject_count_map);
+		PrintLectureList(lecture_list);
+		Double initial_spacing_score = Spacing(lecture_list);
+		System.out.println(initial_spacing_score);
 		Permute(lecture_list, 0);
 		PrintRankedTimeTables(m_ranked_timetable);
 	}
 	
+	private void PrintLectureList(ArrayList<Integer> lecture_list) {
+		for (Integer integer : lecture_list) {
+			String subject = m_subject_code.get(integer);
+			System.out.print(subject + " ");
+		}
+		System.out.println("");
+	}
+
+	private ArrayList<Integer> CreateLectureList2(
+			HashMap<String, Integer> subject_count_map) {
+		ArrayList<Integer> lecture_list = new ArrayList<Integer>();
+		int total_lecture_count = 0;
+		for(Map.Entry<String, Integer> entry : subject_count_map.entrySet()){
+			int current_lecture_count = entry.getValue();
+			total_lecture_count = total_lecture_count+current_lecture_count;
+		}
+		ArrayList<Integer> empty_lecture_slots = new ArrayList<Integer>();
+		for (int i = 0; i < total_lecture_count; i++){
+			empty_lecture_slots.add(i);
+			lecture_list.add(-1);
+		}
+		int subject_code = 0;
+		for(Map.Entry<String, Integer> entry : subject_count_map.entrySet()){
+			int current_lecture_count = entry.getValue();
+			String current_subject = entry.getKey();
+			m_subject_code.put(subject_code, current_subject);
+			int next_distance = Round((float)total_lecture_count/current_lecture_count);
+			int first_empty_location_index = FirstEmptyLectureSlot(empty_lecture_slots);
+			int current_filled_location = empty_lecture_slots.get(first_empty_location_index);
+			SetLectureList(first_empty_location_index, subject_code, empty_lecture_slots, lecture_list);
+			for (int j = 1; j < current_lecture_count; j++) {
+				int next_potential_location = current_filled_location + next_distance;
+				int next_potential_location_index = -1;
+				if(IsNextPotentialLocationOccupied(next_potential_location, empty_lecture_slots) == true)
+					next_potential_location_index = NextClosestEmptyLocation(0, empty_lecture_slots.size()-1, 
+							next_potential_location, empty_lecture_slots);
+				else
+					next_potential_location_index = empty_lecture_slots.indexOf(next_potential_location);
+				SetLectureList(next_potential_location_index, subject_code, empty_lecture_slots, lecture_list);
+				current_filled_location = next_potential_location;
+			}
+			subject_code++;
+		}
+		return lecture_list;
+	}
+
+	private void SetLectureList(int next_empty_location_index, int subject_code,
+			ArrayList<Integer> empty_lecture_slots,
+			ArrayList<Integer> lecture_list) {
+		int empty_location = empty_lecture_slots.get(next_empty_location_index);
+		lecture_list.set(empty_location, subject_code);
+		empty_lecture_slots.remove(next_empty_location_index);
+	}
+
+	private int NextClosestEmptyLocation(int start_index, int end_index,
+			int next_potential_location, ArrayList<Integer> empty_lecture_slots) {
+		if(start_index == end_index){
+			int current_index = start_index;
+			int current_value =  empty_lecture_slots.get(current_index);
+			if(current_index + 1 == empty_lecture_slots.size())
+				return current_index;
+			int next_value = empty_lecture_slots.get(current_index+1);
+			if((next_potential_location-current_value) < (next_value-next_potential_location))
+				return current_index;
+			else
+				return current_index+1;
+		}
+		int mid = (start_index + end_index)/2;
+		int mid_value = empty_lecture_slots.get(mid);
+		if(mid_value > next_potential_location)
+			return NextClosestEmptyLocation(start_index, mid, next_potential_location, empty_lecture_slots);
+		else 
+			return NextClosestEmptyLocation(mid+1, end_index, next_potential_location, empty_lecture_slots);
+	}
+
+	private boolean IsNextPotentialLocationOccupied(
+			int next_potential_location, ArrayList<Integer> empty_lecture_slots) {
+		int index = Collections.binarySearch(empty_lecture_slots, next_potential_location);
+		if(index >= 0)
+			return false;
+		else
+			return true;
+	}
+
+	private int FirstEmptyLectureSlot(ArrayList<Integer> empty_lecture_slots) {
+		return 0;
+	}
+
+	private int Round(float i) {
+		return java.lang.Math.round(i);
+	}
+
 	private ArrayList<Integer> CreateLectureList(HashMap<String, Integer> subject_count_map) {
 		ArrayList<Integer> lecture_list = new ArrayList<Integer>();
 		int i = 0;
@@ -131,6 +227,11 @@ public class SpacedOut {
 		return lecture_list;
 	}
 	public CombinatorialScore Permute(ArrayList<Integer> lecture_list, int i){
+		if(IsPermuteMemoryMember(lecture_list, i)){
+			CombinatorialScore combinatorial_score = BestCombinatorialScore(lecture_list, i);
+			return combinatorial_score;
+		}
+		
 		if(IsBestCombinatorialScore(lecture_list, i)){
 			CombinatorialScore combinatorial_score = BestCombinatorialScore(lecture_list, i);
 			Double node_best_combinatorial_score = NodeBestCombinatorialScore(lecture_list, i, combinatorial_score);
@@ -149,6 +250,8 @@ public class SpacedOut {
 		else if(i==lecture_list.size()){
 			m_permuation_count++;
 			Double score = Spacing(lecture_list);
+			String new_permute_memory_member = lecture_list.toString() + Integer.toString(i);
+			m_permute_memory.add(new_permute_memory_member);
 			UpdateRankedTimeTables(lecture_list, score);
 			return BaseCombinatorialScore();
 		}
@@ -182,8 +285,43 @@ public class SpacedOut {
 		}
 		String combination = SortedLectureList(right);
 		UpdateBestCombinatorialScore(combination, max_combinatorial_score);
-		System.out.println(lecture_list.toString() + " : " + i + " : " + combination + " : " + max_combinatorial_score.RawCombinatorialScore());
+//		System.out.println(lecture_list.toString() + " : " + i + " : " + combination + " : " + max_combinatorial_score.RawCombinatorialScore());
 		return max_combinatorial_score;
+	}
+
+	public CombinatorialScore PermuteSimple(ArrayList<Integer> lecture_list, int i){
+		
+		if(IsPermuteMemoryMember(lecture_list, i)){
+			return new CombinatorialScore();
+		}
+		if(i==lecture_list.size()){
+			m_permuation_count++;
+			String new_permute_memory_member = lecture_list.toString() + Integer.toString(i);
+			m_permute_memory.add(new_permute_memory_member);
+			Double score = Spacing(lecture_list);
+			UpdateRankedTimeTables(lecture_list, score);
+			return new CombinatorialScore();
+		}
+		
+		//System.out.println("===========================");
+		
+		for (int j = i; j < lecture_list.size(); j++) {
+			ArrayList<Integer> new_lecture_list = new ArrayList<Integer>(lecture_list);
+			int first_value = new_lecture_list.get(i).intValue();
+			int second_value = new_lecture_list.get(j).intValue();
+			if( first_value != second_value || i == j){
+				new_lecture_list = LectureSwap(new_lecture_list, i, j);
+				PermuteSimple(new_lecture_list, i + 1); // + SpaceCurrent(new_lecture_list, i);
+			}
+		}
+		return new CombinatorialScore();
+	}	
+	
+	private boolean IsPermuteMemoryMember(ArrayList<Integer> lecture_list, int i) {
+		String permute_memory_member = lecture_list.toString() + Integer.toString(i);
+		if(m_permute_memory.contains(permute_memory_member))
+			return true;
+		return false;
 	}
 
 	private String SortedLectureList(ArrayList<Integer> right) {
@@ -258,8 +396,8 @@ public class SpacedOut {
 		Arrays.sort(chars);
 		String sub_node_structure_sorted = new String(chars);
 		boolean result = m_best_combinatorial_scores.containsKey(sub_node_structure_sorted);
-		if(result == true)
-			System.out.println("Sorted " + sub_node_structure_sorted + " found");
+//		if(result == true)
+//			System.out.println("Sorted " + sub_node_structure_sorted + " found");
 		return result;
 	}
 
@@ -268,7 +406,7 @@ public class SpacedOut {
 		Arrays.sort(chars);
 		String sorted = new String(chars);
 		m_best_combinatorial_scores.put(sorted, max);
-		System.out.println("Updated best combinatorial score for " + sorted + " as " + max.RawCombinatorialScore());
+//		System.out.println("Updated best combinatorial score for " + sorted + " as " + max.RawCombinatorialScore());
 	}
 
 	private Double SpacingBetween(int first_lecture, ArrayList<Integer> second_lecture_set) {
@@ -294,7 +432,10 @@ public class SpacedOut {
 			Integer lecture_key = lecture_list.get(i);
 			if(last_element_index.containsKey(lecture_key)){
 				int last_position = last_element_index.get(lecture_key).intValue();
-				score = score + Math.sqrt(i-last_position);
+				double difference = i-last_position;
+				if(difference == 1)
+					difference = 0.1;
+				score = score + Math.sqrt(difference);
 				last_element_index.put(lecture_key, i);
 			}
 			else
